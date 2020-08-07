cb9c57caabb4faa1b3a8fdd4a7cae2fd02983946
Tomas Diaz <tidiazp@MACPRO-TIDIAZP.local>
2020-08-06 23:16:36 -0400

    add new scroll events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DogService } from './dog/dog.service';
 import { Dog } from './dog/idog';
 import { fromEvent } from 'rxjs/internal/observable/fromEvent';
-import { debounceTime, debounce, throttleTime, map, delay } from 'rxjs/operators';
+import { debounceTime, debounce, throttleTime, map, delay, filter, distinctUntilChanged, tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,39 +11,56 @@ import { debounceTime, debounce, throttleTime, map, delay } from 'rxjs/operators
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('input') input: ElementRef;
+
+
   title = 'sync-scroll';
   quadrants: Array<Dog[]>;
-  // colums: HTMLCollectionOf<Element>;
-
+  subscriptionDebounce: Subscription;
+  subscriptionThrottle: Subscription;
+  subscriptionDelay: Subscription;
+  subscriptionRegular:Subscription;
 
   constructor(private dogservice: DogService) {
 
-    
+
   }
 
   ngOnInit(): void {
     this.dogservice.get(1).subscribe((data) => {
       this.quadrants = this.chunk(data, 25);
+
+      // this.subscribeScrollEventsRegular()
+
       setTimeout(() => {
-        this.subscribeScrollEvents()  
-      }, 100);
+        this.subscribeScrollEventsTrottle();
+        this.subscribeScrollEventsDebounce();
+        this.subscribeScrollEventsDelay();  
+        this.subscribeScrollEventsRegular();  
+      }, 200);
       
+
+
     });
   }
 
+  ngAfterViewInit() {
 
-  subscribeScrollEvents() {
+  }
 
-    let columns = document.getElementsByClassName('flex-column');
+  subscribeScrollEventsRegular() {
+
+    let columns = document.getElementsByClassName('regular');
 
     for (const column of Array.from(columns)) {
 
-      fromEvent(column, 'scroll').pipe(
-        throttleTime(10),
+      const subs = fromEvent(column, 'scroll').pipe(
         map((data) => {
           return data;
         }),
-      ).subscribe(
+      )
+
+      this.subscriptionRegular = subs.subscribe(
         (x: any) => {
           for (const column of Array.from(columns)) {
             column.scrollLeft = x.srcElement.scrollLeft;
@@ -52,6 +70,79 @@ export class AppComponent implements OnInit {
       );
     }
   }
+
+  subscribeScrollEventsDelay() {
+
+    let columns = document.getElementsByClassName('delay');
+
+    for (const column of Array.from(columns)) {
+
+      const subs = fromEvent(column, 'scroll').pipe(
+        delay(1000),
+        map((data) => {
+          return data;
+        }),
+      )
+
+      this.subscriptionDelay = subs.subscribe(
+        (x: any) => {
+          for (const column of Array.from(columns)) {
+            column.scrollLeft = x.srcElement.scrollLeft;
+            column.scrollTop = x.srcElement.scrollTop;
+          }
+        }
+      );
+    }
+  }
+
+  subscribeScrollEventsDebounce() {
+
+    let columns = document.getElementsByClassName('debounce');
+
+    for (const column of Array.from(columns)) {
+
+      const subs = fromEvent(column, 'scroll').pipe(
+        debounceTime(1000),
+        map((data) => {
+          return data;
+        }),
+      )
+
+      this.subscriptionDelay = subs.subscribe(
+        (x: any) => {
+          for (const column of Array.from(columns)) {
+            column.scrollLeft = x.srcElement.scrollLeft;
+            column.scrollTop = x.srcElement.scrollTop;
+          }
+        }
+      );
+    }
+  }
+
+  subscribeScrollEventsTrottle() {
+
+    let columns = document.getElementsByClassName('throttle');
+
+    for (const column of Array.from(columns)) {
+
+      const subs = fromEvent(column, 'scroll').pipe(
+        throttleTime(1000),
+        map((data) => {
+          return data;
+        }),
+      )
+
+      this.subscriptionThrottle = subs.subscribe(
+        (x: any) => {
+          for (const column of Array.from(columns)) {
+            column.scrollLeft = x.srcElement.scrollLeft;
+            column.scrollTop = x.srcElement.scrollTop;
+          }
+        }
+      );
+    }
+  }
+
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpRequestInterceptor } from './http-request.interceptor';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule,
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
